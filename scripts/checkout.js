93178438c9e17f32b28c086c14a381c54230b264
Benjamin Asjali <beasjali@addu.edu.ph>
2026-08-22 12:11:32 +0800

    added localStorage for cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -108,7 +108,6 @@ document.querySelectorAll('.js-delete-link')
       
       const container = document.querySelector(`
         .js-cart-item-container-${productId}`);
-      console.log(container)
       container.remove();
     })
   })
