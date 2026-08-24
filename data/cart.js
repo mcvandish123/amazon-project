50603a262ece8d6d0d13af6aa0116bd32a608cb9
Benjamin Asjali <beasjali@addu.edu.ph>
2026-08-24 11:01:55 +0800

    delivery options interactivity

--- data/cart.js
+++ data/cart.js
@@ -81,3 +81,17 @@ export function updateQuantity(productId, newValue) {
 
   saveToStorage();
 }
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
+
+  saveToStorage();
+}
