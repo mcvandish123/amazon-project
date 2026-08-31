b3348b83b31f7b4e09f1f917003b4d2971bb8ea3
Benjamin Asjali <beasjali@addu.edu.ph>
2026-08-31 12:44:02 +0800

    added an http request for products

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,11 +1,13 @@
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
-import '../data/car.js';
-import '../data/backend.js'
+import { loadProducts } from '../data/products.js';
+//import '../data/car.js';
+// import '../data/backend.js'
 // import '../data/cart-class.js';
 
-
-renderCheckoutHeader();
-renderOrderSummary();
-renderPaymentSummary();
+loadProducts(() => {
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+});
