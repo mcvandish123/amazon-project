ac7c8a9311db396b2ddb701376108ea18bd53fb7
Benjamin Asjali <beasjali@addu.edu.ph>
2026-08-31 08:14:56 +0800

    created the product and clothing class

--- data/products.js
+++ data/products.js
@@ -34,6 +34,28 @@ class Product {
   getPrice() {
     return `$${formatCurrency(this.priceCents)}`
   }
+
+  extraInfoHTML() {
+    return '';
+  }
+
+}
+
+class Clothing extends Product {
+  sizeChartLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() {
+    
+    return `
+      <a href="${this.sizeChartLink}" target="_blank">
+      Size chart</a>
+    `;
+  }
 }
 
 export const products = [
@@ -714,5 +736,8 @@ export const products = [
     priceCents: 1449
   }
 ].map((productDetails) => {
+  if (productDetails.type === 'clothing') {
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
